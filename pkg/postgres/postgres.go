c5b9c731f2ea73af9c3a7d0632f8ee16bd090c0d
gunawanPras <gunawan.anonym741@gmail.com>
2022-08-11 03:10:20 +0700

    merge v3.0

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strconv"
 
+	"gitlab.com/cinco/app/model"
 	"gitlab.com/cinco/configs"
 
 	"gorm.io/driver/postgres"
@@ -34,30 +35,11 @@ func ConnectDB() *gorm.DB {
 
 	fmt.Println("Connection Opened to Database")
 
-	// Migrate the database
-<<<<<<< HEAD
-	//<<<<<<< HEAD
-	//	err = db.AutoMigrate(&model.Account{}, &model.User{}, &model.Cashflow{})
-	//	if err != nil {
-	//		panic("[Gorm] Database failed to migrate!")
-	//	}
-	//
-	//	fmt.Println("Database Migrated")
-=======
-	// err = db.AutoMigrate(&model.Account{}, &model.User{}, &model.Cashflow{})
-	// if err != nil {
-	// 	panic("[Gorm] Database failed to migrate!")
-	// }
+	err = db.AutoMigrate(&model.Account{}, &model.User{}, &model.Cashflow{})
+	if err != nil {
+		panic("[Gorm] Database failed to migrate!")
+	}
 
-	// fmt.Println("Database Migrated")
->>>>>>> origin/feature/hansen
+	fmt.Println("Database Migrated")
 	return db
-	//=======
-	//err = DB.AutoMigrate(&model.Account{}, &model.User{}, &model.Cashflow{})
-	//if err != nil {
-	//	panic("[Gorm] Database failed to migrate!")
-	//}
-	//
-	//fmt.Println("Database Migrated")
-	//>>>>>>> feature/registerLogin/v1/cinco
 }
