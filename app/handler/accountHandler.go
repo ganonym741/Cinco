fceccfdd8bbc2bc227969e624952d1b39ab6bd84
YAgungS <agung.johanes@mail.com>
2022-08-11 10:25:44 +0700

    - html user activation view

--- app/handler/accountHandler.go
+++ app/handler/accountHandler.go
@@ -1,10 +1,8 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"gitlab.com/cinco/app/service/interfaces"
-	utilities "gitlab.com/cinco/utils"
 )
 
 type CincoAccount interface {
@@ -17,24 +15,38 @@ type AccountHandler struct {
 }
 
 func (a AccountHandler) AccountActivation(c *fiber.Ctx) error {
-	fmt.Printf("TEST %s ", c.Params("userId"))
 	userUUID := c.Params("userId")
 
 	if userUUID != "" {
 		user := a.UserService.FindByID(userUUID)
 
 		if len(user.Id) == 0 || user.Id == "" {
-			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": fiber.StatusNotFound, "message": "User not found"})
+			return c.Render("notification", fiber.Map{
+				"name":    user.Fullname,
+				"message": "User not found",
+				"closure": "please contact your system administrator. Good Luck.",
+			})
+			//return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"status": fiber.StatusNotFound, "message": "User not found"})
 		} else {
 			if user.Status == true {
-				utilities.SendMail(user.Email, "your account is already activated, please contact system administrator.")
-				return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusConflict, "message": "user already activated"})
+				//utilities.SendMail(user.Email, "your account is already activated, please contact system administrator.")
+				//return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": fiber.StatusConflict, "message": "user already activated"})
+				return c.Render("notification", fiber.Map{
+					"name":    user.Fullname,
+					"message": "your account is already activated",
+					"closure": "please contact your system administrator. Good Luck.",
+				})
 			}
 			err := a.AccountService.CreateAccount(userUUID)
 
 			if err != nil {
-				utilities.SendMail(user.Email, "error in creating account, please contact system administrator.")
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": err.Error()})
+				//utilities.SendMail(user.Email, "error in creating account, please contact system administrator.")
+				//return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": err.Error()})
+				return c.Render("notification", fiber.Map{
+					"name":    user.Fullname,
+					"message": "error in creating account",
+					"closure": "please contact your system administrator, Good Luck.",
+				})
 			}
 
 			user.Status = true
@@ -42,14 +54,22 @@ func (a AccountHandler) AccountActivation(c *fiber.Ctx) error {
 			err = a.UserService.Update(user)
 
 			if err != nil {
-				utilities.SendMail(user.Email, "failed to activate your account, please contact system administrator.")
-
-				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": err.Error()})
+				//utilities.SendMail(user.Email, "failed to activate your account, please contact system administrator.")
+				//return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": fiber.StatusInternalServerError, "message": err.Error()})
+				return c.Render("notification", fiber.Map{
+					"name":    user.Fullname,
+					"message": "failed to activate your account",
+					"closure": "please contact system administrator, Good Luck.",
+				})
 			}
 
-			utilities.SendMail(user.Email, "your account has been activated successfully.")
-
-			return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "User data has been activated successfully."})
+			//utilities.SendMail(user.Email, "your account has been activated successfully.")
+			//return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "User data has been activated successfully."})
+			return c.Render("notification", fiber.Map{
+				"name":    user.Fullname,
+				"message": "your account has been activated successfully.",
+				"closure": "Good Luck! Hope it works.",
+			})
 		}
 	}
 
